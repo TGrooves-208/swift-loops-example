f225db89d2e2ca2d4173cebe11554b4100b1f31a
TGrooves-208 <tgrooves.208@gmail.com>
2015-12-25 22:59:01 -0700

    3 Various Ways of Performing the same looping function

--- old-skool-mac/ViewController.swift
+++ old-skool-mac/ViewController.swift
@@ -2,6 +2,7 @@
 //  ViewController.swift
 //  old-skool-mac
 //
+//  3 Various ways of performing the same loop
 //  Created by Gil Aguilar on 12/25/15.
 //  Copyright © 2015 toplevel. All rights reserved.
 //
@@ -9,10 +10,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var mainLbl: UILabel!
+    
+    var phrases = ["Booting from floopy...\n","Reading from disk...\n","Updating Registry...\n", ".......\n","...........\n","................\n","Welcome, Fellow Nerd. \nIt is nice to see you again"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mainLbl.text = ""
+//        for var phrase = 0; phrase < phrases.count; phrase++ {
+//            var txt = mainLbl.text!
+//            txt += phrases[phrase]
+//            mainLbl.text = txt
+//        }
+        
+//        var x = 0
+//        repeat {
+//            var txt = mainLbl.text!
+//            txt += phrases[x]
+//            mainLbl.text = txt
+//            x++
+//        } while x < phrases.count
+        for phrase in phrases {
+            var txt = mainLbl.text!
+            txt += phrase
+            mainLbl.text = txt
+        }
     }
 
     override func didReceiveMemoryWarning() {
